219230d20e600d9d0c25713f7653a2b122ba8963
sanghyun han <gkstkdgus98@gmail.com>
2025-09-23 18:28:36 +0900

    공격 애니메이션 코드 추가
    위에 run 애니메이션과 동일한 코드로 공격하는 애니메이션 코드 추가

--- 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
+++ 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
@@ -23,11 +23,18 @@ for x in range(100):
     frame = (frame + 1) % frame_count
     delay(0.05)
 
-run_attack = load_image('run&attack.png')
+run_attack = load_image('run&attack_axe.png')
 run_attack_frame_count = 4
 run_attack_frame_width = run_attack.w // run_attack_frame_count
 run_attack_frame_height = run_attack.h
 
+run_attack_frame = 0
+for x in range(100, 200):
+    clear_canvas()
+    run_attack.clip_draw(run_attack_frame * run_attack_frame_width, 0, run_attack_frame_width, run_attack_frame_height, x, 90)
+    update_canvas()
+    run_attack_frame = (run_attack_frame + 1) % run_attack_frame_count
+    delay(0.05)
 
 
 close_canvas()
