ba5b69b947372fd81c7249ba47d8b7de375ab12f
sanghyun han <gkstkdgus98@gmail.com>
2025-09-23 17:56:31 +0900

    기존 run 애니에서 참고
    대신 내가 갖고있는 axe_run 애니의 프레임이 8개니 8개의 프레임으로 제작

--- 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
+++ 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
@@ -3,4 +3,27 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('')
+character = load_image('alxe_sprite_sheets_final_png.png')
+
+from pico2d import *
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('run_axe.png')
+
+frame_count = 8
+frame_width = character.w // frame_count
+frame_height = character.h //
+
+frame = 0
+x = 400
+
+for x in range(0,50, 903):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+close_canvas()
