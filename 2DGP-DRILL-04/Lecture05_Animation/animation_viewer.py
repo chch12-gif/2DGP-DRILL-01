7e7e4154af0fec3c01a30d63d417f8bf9af4f9b9
sanghyun han <gkstkdgus98@gmail.com>
2025-09-23 18:03:23 +0900

    grass 삭제
    grass. png는 필요 없다고 판단하여 삭제

--- 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
+++ 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
@@ -2,13 +2,11 @@ from pico2d import *
 
 open_canvas()
 
-grass = load_image('grass.png')
-character = load_image('alxe_sprite_sheets_final_png.png')
+character = load_image('run_axe.png')
 
 from pico2d import *
 
 open_canvas()
-grass = load_image('grass.png')
 character = load_image('run_axe.png')
 
 frame_count = 8
@@ -20,7 +18,6 @@ x = 400
 
 for x in range(100):
     clear_canvas()
-    grass.draw(400, 30)
     character.clip_draw(frame * frame_width, 0, frame_width, frame_height, x, 90)
     update_canvas()
     frame = (frame + 1) % frame_count
