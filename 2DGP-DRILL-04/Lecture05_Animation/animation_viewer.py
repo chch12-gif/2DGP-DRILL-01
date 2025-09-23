69214d001d065dd1e4a3f95681dcacc932d011bf
sanghyun han <gkstkdgus98@gmail.com>
2025-09-23 18:00:45 +0900

    오타 수정 및 좌표 수정
    오타를 수정하고 새로운 이미지에 맞는
    프레임 너비 수정

--- 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
+++ 2DGP-DRILL-04/Lecture05_Animation/animation_viewer.py
@@ -13,17 +13,17 @@ character = load_image('run_axe.png')
 
 frame_count = 8
 frame_width = character.w // frame_count
-frame_height = character.h //
+frame_height = character.h
 
 frame = 0
 x = 400
 
-for x in range(0,50, 903):
+for x in range(100):
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    character.clip_draw(frame * frame_width, 0, frame_width, frame_height, x, 90)
     update_canvas()
-    frame = (frame + 1) % 8
+    frame = (frame + 1) % frame_count
     delay(0.05)
 
 close_canvas()
