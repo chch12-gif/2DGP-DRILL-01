4a9c12ccccb70da59a88dfa2d9621a5ab020b16c
sanghyun han <gkstkdgus98@gmail.com>
2025-10-28 17:57:11 +0900

    Update grass.py
    도저희 grass가 짤리는 원인을 시간내에 못찾았다.

--- Lecture10_Game_World/grass.py
+++ Lecture10_Game_World/grass.py
@@ -5,7 +5,7 @@ class Grass:
     def __init__(self, x=400, y=30):
         self.image = load_image('grass.png')
         self.x= x
-        self.y= y
+        self.y= y + self.image.h // 2
 
     def draw(self):
         self.image.draw(self.x, self.y)
