9fb1aa817a6136336ca19e243a548166a8041b7a
sanghyun han <gkstkdgus98@gmail.com>
2025-10-28 17:42:20 +0900

    Update grass.py
    grass가 x,y의 인수를 받을수 있게 코드 변경

--- Lecture10_Game_World/grass.py
+++ Lecture10_Game_World/grass.py
@@ -2,10 +2,10 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self):
+    def __init__(self, x=400, y=30):
         self.image = load_image('grass.png')
-        self.x= 400
-        self.y= 30
+        self.x= x
+        self.y= y
 
     def draw(self):
         self.image.draw(self.x, self.y)
