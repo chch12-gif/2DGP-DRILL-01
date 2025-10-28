c2efbdf665a19506ec894eb60bbdbfd651147f77
sanghyun han <gkstkdgus98@gmail.com>
2025-10-28 17:30:43 +0900

    Update grass.py
    400,30에 grass 배치

--- Lecture10_Game_World/grass.py
+++ Lecture10_Game_World/grass.py
@@ -4,9 +4,11 @@ from pico2d import load_image
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
+        self.x= 400
+        self.y= 30
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(self.x, self.y)
 
     def update(self):
         pass
